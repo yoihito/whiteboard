8cb77763c5b59d4c6b366395b2fdc7b40325b931
Vadim Gribanov <v1m@tut.by>
2015-03-15 19:52:21 +0300

    Small server refactoring

--- app/socket.js
+++ app/socket.js
@@ -1,6 +1,7 @@
 var async = require('async');
 var Promise = require('es6-promise').Promise;
 var keyMirror = require('react/lib/keyMirror');
+var io = require('socket.io');
 var SocketActionTypes = keyMirror({
     REMOTE_BEGIN_DRAWING: null,
     REMOTE_MOVE_CURSOR: null,
@@ -17,35 +18,34 @@ function emitAction(socket, action, data) {
   }
 }
 
+
+function socketEvent(socket, e, callback) {
+  if (Array.isArray(e)) {
+    e.forEach(function(es) {
+      socket.on(es, function(message) {
+        callback(es, message);
+      });
+    });
+  } else {
+    socket.on(e, function(message) {
+      callback(e, message);
+    });
+  }
+}
+
 var SockServer = function(server) {
-  io = require('socket.io')(server);
-  io.on('connection', function (socket) {
+  websockServer = io(server);
+  websockServer.on('connection', function (socket) {
     _sockets.push(socket);
-    socket.on(SocketActionTypes.REMOTE_BEGIN_DRAWING, function (message) {
+    socketEvent(socket,[
+      SocketActionTypes.REMOTE_BEGIN_DRAWING,
+      SocketActionTypes.REMOTE_MOVE_CURSOR,
+      SocketActionTypes.REMOTE_END_DRAWING
+    ], function (e, message) {
       async.each(_sockets,
         emitAction(
           socket,
-          SocketActionTypes.REMOTE_BEGIN_DRAWING,
-          {from: socket.id, point: message}
-        )
-      );
-    });
-
-    socket.on(SocketActionTypes.REMOTE_MOVE_CURSOR, function (message) {
-      async.each(_sockets,
-        emitAction(
-          socket,
-          SocketActionTypes.REMOTE_MOVE_CURSOR,
-          {from: socket.id, point: message}
-        )
-      );
-    });
-
-    socket.on(SocketActionTypes.REMOTE_END_DRAWING, function (message) {
-      async.each(_sockets,
-        emitAction(
-          socket,
-          SocketActionTypes.REMOTE_END_DRAWING,
+          e,
           {from: socket.id, point: message}
         )
       );
--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -8,3 +8,5 @@ React.render(
   <Whiteboard/>,
   document.getElementById('main')
 );
+
+
